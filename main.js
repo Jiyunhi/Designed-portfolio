e4f4b1c2cae3a1979c9d9fe3247025b359947b3a
Jiyunhi <104748283+Jiyunhi@users.noreply.github.com>
2022-05-05 23:54:06 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-/*header gnb*/
-
-$("#header #menu li").click( function(){
-    var da = $(this).attr("data-anchor");
-       $(".fp-viewing-"+da+" #header #menu li").addClass("active").siblings("li").removeClass("active");
-});
